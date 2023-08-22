66ba160025ca7ed04ef9dfbd85240efd9f347c31
MariaGlushtsova <glushtsova@tut.by>
2023-08-22 22:59:04 +0300

    Additional task

--- HW19-IOS-MariaGlushtsova.playground/Contents.swift
+++ HW19-IOS-MariaGlushtsova.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CryptoKit
 
 func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
@@ -22,3 +23,21 @@ getData(urlRequest: "https://www.fruivice.com/api/fruit/Kiwi")
 
 // Ввела верный API
 getData(urlRequest: "https://www.fruityvice.com/api/fruit/Kiwi")
+
+// MARK: - Задание со звездочкой
+
+func MD5(string: String) -> String {
+    let digest = Insecure.MD5.hash(data: Data(string.utf8))
+
+    return digest.map {
+        String(format: "%02hhx", $0)
+    }.joined()
+}
+
+MD5(string: "1ac631c7950814dd154df38478204f4363aaa13dd17f5b878ef1fe2f49a005a90a9967aa9")
+
+// Ошибка в имени домена
+getData(urlRequest: "https://gateway.mavel.com/v1/public/characters/1009229?ts=1&apikey=17f5b878ef1fe2f49a005a90a9967aa9&hash=fa3a74f0e4fcd5aecc20f1516bc66ed4")
+
+// Ввела верный API
+getData(urlRequest: "https://gateway.marvel.com/v1/public/characters/1009229?ts=1&apikey=17f5b878ef1fe2f49a005a90a9967aa9&hash=fa3a74f0e4fcd5aecc20f1516bc66ed4")
